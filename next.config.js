8842970eabf87c1538ec7c9912462419097178df
PeaceCwz (Baris Ceviz) <me@barisceviz.com>
2020-10-01 07:58:47 +0300

    Remove prefix

--- next.config.js
+++ next.config.js
@@ -6,12 +6,7 @@ const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/,
 });
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-    assetPrefix: isProduction
-        ? "https://peacecwz.github.io/barisceviz.com/"
-        : "",
     webpack: function (config) {
         config.module.rules.push({
             test: /\.md$/,
