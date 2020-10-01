9ba24cdd155b5e148b8646d2d65339a3cda5aa04
PeaceCwz (Baris Ceviz) <me@barisceviz.com>
2020-10-01 05:39:11 +0300

    Add assets prefix

--- next.config.js
+++ next.config.js
@@ -1,21 +1,35 @@
-const withPlugins = require('next-compose-plugins')
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withOptimizedImages = require('next-optimized-images')
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const withOptimizedImages = require("next-optimized-images");
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+});
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  webpack: function(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
-  },
-}
+    assetPrefix: isProduction
+        ? "https://peacecwz.github.io/barisceviz.com/"
+        : "",
+    webpack: function (config) {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: "raw-loader",
+        });
+        return config;
+    },
+};
 
-module.exports = withPlugins([withCSS, withSass, withOptimizedImages, withMDX, {
-  pageExtensions: ['js', 'jsx', 'mdx', 'md', 'tsx'],
-}], nextConfig)
+module.exports = withPlugins(
+    [
+        withCSS,
+        withSass,
+        withOptimizedImages,
+        withMDX,
+        {
+            pageExtensions: ["js", "jsx", "mdx", "md", "tsx"],
+        },
+    ],
+    nextConfig
+);
